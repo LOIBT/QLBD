0aa7785c737f6e7f40efe119b9c33b5f534f74d5
LOIBT <loibt12a1@gmail.com>
2022-05-24 14:26:39 +0700

    add file

---
+++ key.py
@@ -0,0 +1,6 @@
+config = {
+    'DSN': "localhost:1521/orcl",                    # vi du "localhost:1521/orcl"
+    'oracle_passwd': "project",           # vi du "123"
+    'oracle_user': "project",                   # vi du "ORCL"
+    'skey': "DA1" 
+}
